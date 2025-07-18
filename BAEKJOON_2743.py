372f63cd88f07c153b20f00cb67013f22e667d11
손재민 <thswoalsrns@naver.com>
2025-07-18 21:28:31 +0900

    25.07.18

--- BAEKJOON_2743.py
+++ BAEKJOON_2743.py
@@ -1,8 +1,19 @@
 import sys
 sys.stdin = open("input.txt",'r')
 
-text = list(input())
-cnt = 0
-for _ in text:
-    cnt += 1
-print(cnt)
+tc = int(input())
+for i in range(tc):
+    text = list(input())
+    cnt = 0
+    for _ in text:
+        cnt += 1
+
+    for _ in range(0,len(text)):
+        if(cnt == 1 ):
+            print(text[_],end='')
+            print(text[_])
+            break
+        if(_ == 0):
+            print(text[_],end='')
+        if(_ == len(text)-1):
+            print(text[_])
