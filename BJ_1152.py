0b30850b6134527408a7ac35daef3cee1f8e95b7
jaemin-hand <thswoalsrns@naver.com>
2025-07-20 23:07:47 +0900

    25.07.20

---
+++ BJ_1152.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+text = list(input())
+cnt = 1
+for i in range(0,len(text)):
+    if(i == 0 and text[i] == ' '):
+        continue
+    if(i == len(text)-1 and text[i] == ' '):
+        break
+    if(text[i] == ' '):
+        cnt += 1
+    
+
+print(cnt)
