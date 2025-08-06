97ac5eed617d823ddd283709e4385cda6fa7de6f
jaemin-hand <thswoalsrns@naver.com>
2025-08-07 03:19:07 +0900

    08.07_Algorithm

--- BJ_10988_2.py
+++ BJ_10988_2.py
@@ -1,5 +1,13 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-insert = list(input())
+insert = input().strip()
 
+result = 1
+
+for i in range(len(insert)//2):
+    if insert[i] != insert[-(i+1)]:
+        result = 0
+        break
+
+print(result)
