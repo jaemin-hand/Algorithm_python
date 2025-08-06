28391fe6ae07f646a3fe705dfba6fcdb7e094755
손재민 <thswoalsrns@naver.com>
2025-08-06 21:46:06 +0900

    08.06_Algorithm

--- BJ_10988.py
+++ BJ_10988.py
@@ -1,14 +1,24 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-N = int(input())
+insert = list(input())
 
-for i in range(1,N+1):
-    star = 2 * i - 1
-    void = N - i
-    print(' ' * void + '*' * star)
+result = 0
 
-for i in range(N-1, 0, -1):
-    star = 2 * i - 1
-    void = N - i
-    print(' ' * void + '*' * star)
+if(len(insert) % 2 == 0): # even
+    for i in range(0,len(insert)//2):
+        if(insert[len(insert)//2 - (i+1)] == insert[len(insert)//2 + i]):
+            result =1
+        else:
+            result = 0
+            break
+else: # odd
+    for i in range(0,len(insert)//2):
+        if(insert[len(insert)//2 - (i+1)] == insert[len(insert)//2 + (i+1)]):
+            result = 1
+        else:
+            result = 0
+            break
+if(len(insert) == 1):
+    result = 1
+print(result)
--- BJ_2444.py
+++ BJ_2444.py
@@ -1,5 +1,14 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-A = int(input())
+N = int(input())
 
+for i in range(1,N+1):
+    star = 2 * i - 1
+    void = N - i
+    print(' ' * void + '*' * star)
+
+for i in range(N-1, 0, -1):
+    star = 2 * i - 1
+    void = N - i
+    print(' ' * void + '*' * star)
