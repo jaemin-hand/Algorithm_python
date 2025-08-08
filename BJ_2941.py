70978c0e1326f28b9e13b363f70f042eb13b0437
jaemin-hand <thswoalsrns@naver.com>
2025-08-08 22:49:16 +0900

    08.08

---
+++ BJ_2941.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+text = input()
+
+cro = {1:'c=',
+       2:'c-',
+       3:'dz=',
+       4:'d-',
+       5:'lj',
+       6:'nj',
+       7:'s=',
+       8:'z='}
+
+for pattern in cro.values():
+    text = text.replace(pattern,"*")
+
+print(len(text))
