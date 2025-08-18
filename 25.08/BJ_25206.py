34adf75599043e815aa679ac2372322e9d3a84ba
손재민 <thswoalsrns@naver.com>
2025-08-18 18:27:56 +0900

    08.18

--- 25.08/BJ_25206.py
+++ 25.08/BJ_25206.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input.txt','r')
 
 # output == (학점 * 등급)
-# ex) 3.0 * A+
+# ex) 3.0 * A+ / sums
 
 N = 20
 
@@ -16,21 +16,25 @@ DIC = {
     'D+' : 1.5,
     'D0' : 1.0
 }
-
+for_result = 0
+sums = 0
 for _ in range(N):
-    cnt = 0
     avg = 0
-    summit = 0
     text = input().split()
-    temp = []
     score = 0
     grade = ''
-    for_result = 0
     # # text[len(text)-6:] == 3.0 A+
+    # ex) 3.0 * A+
+    # 전공평점 == 3.0 * A+ / sums
     if(text[-1] != 'P'):
         score = (float(text[1]))
         grade = text[2]
-        cnt += 1
-        for_result = score * DIC[grade]
-    avg = for_result / cnt
-    print(avg)
+        sums += score
+        if(grade != 'F'):
+            for_result += score * DIC[grade]
+if(sums == 0):
+    nums = 0.0
+    print("{:.6f}".format(nums))
+else:
+    avg = for_result / sums
+    print("{:.6f}".format(avg))
