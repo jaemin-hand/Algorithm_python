d57071c921e21c34386fb858edb693e77eb0a56f
jaemin-hand <thswoalsrns@naver.com>
2025-10-14 04:54:45 +0900

    10.14_dfs_practice

--- 25.10/2606.py
+++ 25.10/2606.py
@@ -2,40 +2,27 @@ import sys
 sys.stdin = open('input.txt','r')
 
 cpu = int(input())
-# 7
 
-line = int(input())
-# 6
+connect = int(input())
 
 node = [[] for _ in range(cpu + 1)]
-# [ [] [] [] [] [] [] [] [] ]
 
-for _ in range(line):
-    a, b = map(int,input().split())
-    # 1 , 2
-
+for _ in range(connect):
+    a,b = map(int,input().split())
     node[a].append(b)
-    # node[1].append(2)
-    # [ [] [1] [] [] [] [] [] [] ]
-
     node[b].append(a)
-    # node[2].append(1)
-    # [ [] [2] [1] [] [] [] [] [] ]
-
-visited = [0] * (cpu + 1)
 
 count = 0
 
+visited = [0] * (cpu + 1)
+
 def dfs(start):
     global count
-    
     visited[start] = 1
-
-    for i in node[start]:
-        if visited[i] == 0:
+    for next in node[start]:
+        if visited[next] == 0:
             count += 1
-            dfs(i)
-    
-dfs(1)
+            dfs(next)
 
+dfs(1)
 print(count)
